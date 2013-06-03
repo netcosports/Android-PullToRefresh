fa56c22e177080a159f66e7b19da2ff35253123d
Stéphane Guérin <guerwan@gmail.com>
2013-06-03 14:42:29 +0200

    added a staggeredGridView with pull to refresh

--- sample/src/com/handmark/pulltorefresh/samples/LauncherActivity.java
+++ sample/src/com/handmark/pulltorefresh/samples/LauncherActivity.java
@@ -25,7 +25,7 @@ import android.widget.ListView;
 public class LauncherActivity extends ListActivity {
 
 	public static final String[] options = { "ListView", "ExpandableListView", "GridView", "WebView", "ScrollView",
-			"Horizontal ScrollView", "ViewPager", "ListView Fragment", "WebView Advanced", "ListView in ViewPager" };
+			"Horizontal ScrollView", "ViewPager", "ListView Fragment", "WebView Advanced", "ListView in ViewPager" , "Staggered GridView"};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -68,6 +68,9 @@ public class LauncherActivity extends ListActivity {
 				break;
 			case 9:
 				intent = new Intent(this, PullToRefreshListInViewPagerActivity.class);
+				break;
+			case 10:
+				intent = new Intent(this, PullToRefreshStaggeredGridActivity.class);
 				break;
 		}
 
